216cdcdbe8973db0b1343f5a038d4e2b1b79aae4
beletate <vinicius_beletate@hotmail.com>
2021-04-21 15:35:33 -0300

    Listando episodios na pagina home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import '../styles/global.scss'
 
 import styles from '../styles/app.module.scss'
-import Header from './components/Header'
-import Player from './components/Player'
+import React from 'react'
+import Header from '../components/Header'
+import Player from '../components/Player'
 
 
 function MyApp({ Component, pageProps }) {
